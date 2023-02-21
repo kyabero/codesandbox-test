3e0cfad8ee3fc2fca61c143eb22c5fd9dc3489f0
kyabero <75917379+kyabero@users.noreply.github.com>
2023-02-21 23:07:53 +0900

    add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import "./styles.css";
+// let a = "あああ";
+// console.log(a);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const val3 = "const変数";
+// console.log(val3);
+
+// const val4 = {
+//   name:"じゃけ",
+//   age:"28",
+// };
+//   val4.addres = "広島";
+
+// console.log(val4);
+const myprofile = ['ジャケ',28];
+
+const [a,b] = myprofile;
+const message4 = `名前は${a}です。${b}です。`;
+console.log(message4);
